0a5bb2162b0a93fe1862f6f05748fba5a7791bac
Amodh Dhakal <amodhakal@gmail.com>
2024-12-24 00:04:45 -0500

    Recognizing Lexems (4.5)

--- src/amodhakal/lox/Scanner.kt
+++ src/amodhakal/lox/Scanner.kt
@@ -1,10 +1,13 @@
 package amodhakal.lox
 
+import com.sun.tools.example.debug.expr.ExpressionParserConstants.BANG
+
+
 class Scanner internal constructor(private val source: String) {
     private val tokens: MutableList<Token> = ArrayList()
     private var start = 0
-    private val current = 0
-    private val line = 1
+    private var current = 0
+    private var line = 1
 
     fun scanTokens(): List<Token> {
         while (!isAtEnd) {
@@ -18,4 +21,46 @@ class Scanner internal constructor(private val source: String) {
 
     private val isAtEnd: Boolean
         get() = current >= source.length
+
+    private fun scanToken() {
+        val ch = advance()
+        when (ch) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG);
+            '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
+            '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
+            '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
+            else -> Lox.error(line, "Unexpected character.")
+        }
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun addToken(type: TokenType) {
+        addToken(type, null)
+    }
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd) return false
+        if (source[current] != expected) return false
+
+        current++
+        return true
+    }
 }
