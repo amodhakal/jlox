0b8b63b7106f6f940e07dc581a8448e7b5390173
Amodh Dhakal <amodhakal@gmail.com>
2024-12-25 20:08:24 -0500

    Removed kotlin code
    Need to replace with java due to CFG code

--- src/amodhakal/lox/Token.kt
+++
@@ -1,7 +0,0 @@
-package amodhakal.lox
-
-class Token internal constructor(val type: TokenType, val lexeme: String, val literal: Any?, val line: Int) {
-    override fun toString(): String {
-        return "$type $lexeme $literal"
-    }
-}
