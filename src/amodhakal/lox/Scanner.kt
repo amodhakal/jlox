8a6ab8ba441820e8facb8476ca457aa606383585
Amodh Dhakal <amodhakal@gmail.com>
2024-12-25 19:45:55 -0500

    Reserved words and identifier

--- src/amodhakal/lox/Scanner.kt
+++ src/amodhakal/lox/Scanner.kt
@@ -1,10 +1,43 @@
 package amodhakal.lox
 
+import com.sun.org.apache.bcel.internal.generic.RETURN
+import com.sun.tools.example.debug.expr.ExpressionParserConstants.ELSE
+import com.sun.tools.example.debug.expr.ExpressionParserConstants.FOR
+import com.sun.tools.example.debug.expr.ExpressionParserConstants.IF
+import com.sun.tools.example.debug.expr.ExpressionParserConstants.WHILE
+import com.sun.tools.javac.code.Kinds.KindSelector.NIL
+import java.util.HashMap
+
+
 class Scanner internal constructor(private val source: String) {
     private val tokens: MutableList<Token> = ArrayList()
     private var start = 0
     private var current = 0
     private var line = 1
+
+    private var keywords: MutableMap<String, TokenType> = initKeywords()
+
+    private fun initKeywords(): MutableMap<String, TokenType> {
+        val keywords = HashMap<String, TokenType>()
+        keywords["and"] = TokenType.AND;
+        keywords["class"] = TokenType.CLASS;
+        keywords["else"] = TokenType.ELSE;
+        keywords["false"] = TokenType.FALSE;
+        keywords["for"] = TokenType.FOR;
+        keywords["fun"] = TokenType.FUN;
+        keywords["if"] = TokenType.IF;
+        keywords["nil"] = TokenType.NIL;
+        keywords["or"] = TokenType.OR;
+        keywords["print"] = TokenType.PRINT;
+        keywords["return"] = TokenType.RETURN;
+        keywords["super"] = TokenType.SUPER;
+        keywords["this"] = TokenType.THIS;
+        keywords["true"] = TokenType.TRUE;
+        keywords["var"] = TokenType.VAR;
+        keywords["while"] = TokenType.WHILE;
+        return keywords
+    }
+
 
     fun scanTokens(): List<Token> {
         while (!isAtEnd) {
@@ -20,8 +53,7 @@ class Scanner internal constructor(private val source: String) {
         get() = current >= source.length
 
     private fun scanToken() {
-        val ch = advance()
-        when (ch) {
+        when (val ch = advance()) {
             '(' -> addToken(TokenType.LEFT_PAREN)
             ')' -> addToken(TokenType.RIGHT_PAREN)
             '{' -> addToken(TokenType.LEFT_BRACE)
@@ -32,7 +64,7 @@ class Scanner internal constructor(private val source: String) {
             '+' -> addToken(TokenType.PLUS)
             ';' -> addToken(TokenType.SEMICOLON)
             '*' -> addToken(TokenType.STAR)
-            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG);
+            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG)
             '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
             '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
             '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
@@ -50,10 +82,20 @@ class Scanner internal constructor(private val source: String) {
 
             else -> if (isDigit(ch)) {
                 number()
+            } else if (isAlpha(ch)) {
+                identifier()
             } else {
                 Lox.error(line, "Unexpected character.")
             }
         }
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+        val text = source.substring(start, current)
+        var type = keywords[text]
+        if (type == null) type = TokenType.IDENTIFIER
+        addToken(type)
     }
 
     private fun number() {
@@ -64,7 +106,7 @@ class Scanner internal constructor(private val source: String) {
             while (isDigit(peek())) advance()
         }
 
-        addToken(TokenType.NUMBER, source.substring(start, current).toDouble());
+        addToken(TokenType.NUMBER, source.substring(start, current).toDouble())
     }
 
     private fun string() {
@@ -112,6 +154,14 @@ class Scanner internal constructor(private val source: String) {
     private fun peekNext(): Char {
         if (current + 1 >= source.length) return '\u0000'
         return source[current + 1]
+    }
+
+    private fun isAlpha(ch: Char): Boolean {
+        return (ch in 'a'..'z') || (ch in 'A'..'Z') || ch == '_'
+    }
+
+    private fun isAlphaNumeric(ch: Char): Boolean {
+        return isAlpha(ch) || isDigit(ch)
     }
 
     private fun isDigit(ch: Char): Boolean {
